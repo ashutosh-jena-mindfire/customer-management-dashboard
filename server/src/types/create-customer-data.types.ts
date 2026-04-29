aea13c3c70b2146eecf7cb187c1622890b5ca6fd
ashutosh.jena <ashutosh.jena@limgeomatics.onmicrosoft.com>
2026-04-29 05:45:50 +0530

    refactor: Remove unused customer data types

--- server/src/types/create-customer-data.types.ts
+++
@@ -1,6 +0,0 @@
-// Define the interface for the incoming creation data
-export interface CreateCustomerData {
-  name: string;
-  email: string;
-  phone: string;
-}
--- server/src/types/customer.types.ts
+++
@@ -1,7 +0,0 @@
-export interface Customer {
-  id: string;
-  name: string;
-  email: string;
-  phone: string;
-}
-
